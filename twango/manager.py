61ec5bdf739b0c7165c8aed85378e0b88c51e920
John Turner <j.turner@mhnltd.co.uk>
2014-08-14 12:20:09 +0100

    Fix for deprecation warning in django 1.7rc2

--- twango/manager.py
+++ twango/manager.py
@@ -4,5 +4,5 @@ from twango.query import TwistedQuerySet
 class TwistedManager(models.Manager):
     queryset_class = TwistedQuerySet
 
-    def get_query_set(self):
+    def get_queryset(self):
         return self.queryset_class(self.model, using=self._db)
